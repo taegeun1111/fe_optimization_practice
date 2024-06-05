578945f852647577dc3953a15bb28b15d917d60c
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2024-06-05 19:12:44 +0900

    FEAT: video poster 적용

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,11 +1,22 @@
-import classNames from 'classnames'
+import classNames from 'classnames/bind'
 import Section from '@shared/Section'
 import styles from './Video.module.scss'
 
 const cx = classNames.bind(styles)
 
 const Video = () => {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
 
 export default Video
